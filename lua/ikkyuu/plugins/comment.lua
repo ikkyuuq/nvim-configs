7fa0a60230b6f6213bc1f7dbb5c7b2ffcf23b91b
Kyuupid <kittipongprasompong@gmail.com>
2024-05-29 22:02:56 +0700

    added comment plugin

---
+++ lua/ikkyuu/plugins/comment.lua
@@ -0,0 +1,12 @@
+return {
+    'numToStr/Comment.nvim',
+    opts = {
+      padding = true,
+      sticky = true,
+      toggler = {
+        line = "cc", -- comemnt line
+        block = "bc", -- block comment
+      },
+    },
+    lazy = false,
+}
