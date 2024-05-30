cd8ea0c670b6d247cd5263acafeeee4d244184fa
Kyuupid <kittipongprasompong@gmail.com>
2024-05-30 09:08:00 +0700

    just delete an empty space

--- lua/ikkyuu/plugins/colors.lua
+++ lua/ikkyuu/plugins/colors.lua
@@ -1,27 +1,26 @@
 return {
-
   {
     "catppuccin/nvim",
     config = function()
       require("catppuccin").setup({
-	integrations = {
-	  cmp = true,
-	  gitsigns = true,
-	  harpoon = true,
-	  mason = true,
-	  native_lsp = { enabled = true },
-	  symbols_outline = true,
-	  telescope = true,
-	  treesitter = true,
-	  treesitter_context = true,
-	},
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          harpoon = true,
+          mason = true,
+          native_lsp = { enabled = true },
+          symbols_outline = true,
+          telescope = true,
+          treesitter = true,
+          treesitter_context = true,
+        },
       })
 
       vim.cmd.colorscheme("catppuccin-macchiato")
 
       -- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
       for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-	vim.api.nvim_set_hl(0, group, {})
+        vim.api.nvim_set_hl(0, group, {})
       end
     end,
   },
